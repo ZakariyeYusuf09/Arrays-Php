9208e4371da1873e082c6793867c1a42068070a2
ZakariyeYusuf09 <zakariyeyuusuf617@gmail.com>
2023-10-29 11:26:37 +0300

    Added Q3 and Q3

--- All.php
+++ All.php
@@ -7,8 +7,8 @@ $decimal = 0;
 $binaryLength = strlen($binary);
 
 for ($i = 0; $i < $binaryLength; $i++) {
-    $bit = $binary[$binaryLength - $i - 1];
-    $decimal += $bit * (2 ** $i);
+    $colors = $binary[$binaryLength - $i - 1];
+    $decimal += $colors * (2 ** $i);
 }
 
 // Decimal to Octal
@@ -36,14 +36,14 @@ echo "Decimal: $decimal<br>";
 echo "Octal: $octal<br>";
 echo "Hexadecimal: $hexadecimal<br>";
 
-echo " <b>2) [A] Question Two</b> Declarig and Printing All elements of the array<br>";
+echo "<b>2) Question 2<br>[A]</b>";
 $arrays = [5, -7, 12, 10, -7, 11, -6, 12, 1, -7, 2, 9];
 foreach($arrays as $array){
     echo $array ."  ";
 
 }
 echo "<br>";
-echo " <b>2) [B] Question Two</b><br>";
+echo " <b>[B]</b>";
 $total1 = 0;
 
 foreach($arrays as $element){
@@ -52,7 +52,7 @@ foreach($arrays as $element){
     }
 }
 echo "The Total Of odd Elements of the array is : $total1" . "<br>";
-echo " <b>2) [C] Question Two</b><br>";
+echo "<b>[C]</b>";
 $total2= 0;
 for($i=0; $i<count($arrays); $i++){
     if($arrays[$i]%2==0){
@@ -61,13 +61,13 @@ for($i=0; $i<count($arrays); $i++){
 }
 echo "The Total Of Even Elements of the array is : $total2";
 echo "<br>";
-echo " <b>2) [D] Question Two</b><br>";
+echo " <b>[D]</b>";
 $total3 =0;
 foreach($arrays as $value){
     $total3+=$value;
 }
 echo "The Total Of All Elements of the array is : $total3" . "<br>";
-echo " <b>2) [E] Question Two</b><br>";
+echo "<b>[E]</b>";
 $arrays = [5, -7, 12, 10, -7, 11, -6, 12, 1, -7, 2, 9];
 
 $smallest = min($arrays); 
@@ -85,7 +85,7 @@ foreach ($positions as $pos){
 }
 echo "<br>";
 
-echo " <b>2) [F] Question Two</b><br>";
+echo " <b>[F]</b>";
 $maximum = max($arrays); 
 $positions = array(); 
 
@@ -103,4 +103,55 @@ echo "<br>";
 
 
 echo "<b>3) Question Three</b><br>";
+     $associativeArray = array(
+        'Id' => 'IT20209290122',
+        'Name' => 'Mohamed Ali Hassan',
+        'Phone' => '0706172827',
+        'Dob' => '25-09-2022',
+        'Sex' => 'Male',
+     );
+    
+    echo ("<table border ='1' width = '60%' cellspacing = '0'>");
+        
+    echo ("<tr style = 'background-color: lightblue;'>");
+    echo ("<th> Index </th>");
+    echo ("<th> Value </th>");
+    echo ("</tr>");
+    foreach ($associativeArray as $k1 => $row) {
+        echo ("<tr><td style = 'background-color: lightgray;'>$k1");
+        echo ("<td>$row");     
+    }
+    echo ("</table>");
+echo "<b>4) Question Four</b><br>";
+$colors = array (
+    'Light' => array (
+        'Red'=> 'Light Red',
+        'Green'=> 'Light Green',
+        'Blue'=> "Light Blue",
+    ),
+    'Normal' => array (
+        'Red'=> 'Norma Light ',
+        'Green'=> 'Normal Green',
+        'Blue'=> "Normal Blue",
+    ),
+    'Dark' => array (
+        'Red' => "Dark Red",
+        "Green"=> "Dark Green",
+        "Blue"=> "Dark Blue",
+    ),
+);
+echo ("<table border ='1' width = '70%' cellspacing = '0'>");
+    foreach ($colors as $k1 => $row) {
+        echo ("<tr style = 'background-color: lightblue;'><th>");
+        foreach ($row as $k2 => $k)
+            echo ("<th>$k2");
+        break;
+    }
+    foreach ($colors as $k1 => $row) {
+        echo ("<tr><th style = 'background-color: lightgray; text-align: left;'>$k1");
+        foreach ($row as $k2 => $k)
+            echo ("<td>$k");
+    }
+
+
 ?>
